b92ddb49451e13abfe8621b51faa574cfcc730c6
flowa-w <florencewambui86@gmail.com>
2024-11-13 06:07:28 +0300

    Add files via upload

---
+++ list.py
@@ -0,0 +1,17 @@
+my_list = []
+
+my_list = [10, 20, 30, 40]
+#print(my_list)
+
+my_list = [10, 15, 20, 30, 40]
+#print(my_list)
+
+my_list =[10, 15, 20, 30, 40, 50, 60, 70]
+#print(my_list)
+
+my_list = [10, 15, 20, 30, 40, 50, 60]
+#print(my_list.sort)
+
+my_list = [10, 15, 20, 30, 40, 50, 60]
+index_of_30 = my_list.index(30)
+print(index_of_30)
